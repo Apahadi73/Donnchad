29f6326f6ba3429d432b1d307cce0bd475362ed1
apahadi73 <apahadi73@gmail.com>
2021-10-21 00:20:04 -0500

    express app wrapped in http server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import colors from "colors";
 import morgan from "morgan";
 import nodeCron from "node-cron";
+import http from "http";
 
 // routes import
 import { userRouter } from "./routes/userRoutes.js";
@@ -23,43 +24,28 @@ const app = express();
 // json body parser middleware
 app.use(express.json());
 
-// we use morgan to log all the incoming request
-app.use(morgan("dev"));
-
-// all routes
-app.use("/api/users", userRouter);
-app.use("/api/events", eventRouter);
-app.use("/api/chats", chatRouter);
-
-app.get("/", async (req, res) => {
-  res.status(200).send("Welcome to the donnchad world.");
-});
-
-// middlewares
-app.use(notFound);
-app.use(errorHandler);
-
+const server = http.Server(app);
 // sets port and listener
 const PORT = process.env.PORT || 5000;
 // only listen if not in test environment
 if (process.env.NODE_ENV !== "test") {
-  app.listen(PORT, async () => {
+  server.listen(PORT, async () => {
     console.log(
       `Server is listening on port: ${process.env.PORT}!`.yellow.bold
     );
-    await migrate();
-    await seed();
-    const dateTime = new Date().toLocaleString().split("/");
-    const scheduleTime = `${dateTime[2].split(",")[0]}-${dateTime[0]}-${
-      dateTime[1]
-    }`;
-    await crawlEvents(scheduleTime);
+    // await migrate();
+    // await seed();
+    // const dateTime = new Date().toLocaleString().split("/");
+    // const scheduleTime = `${dateTime[2].split(",")[0]}-${dateTime[0]}-${
+    //   dateTime[1]
+    // }`;
+    // await crawlEvents(scheduleTime);
 
-    const job = nodeCron.schedule("30 20 * * * *", async () => {
-      await crawlEvents(scheduleTime);
-    });
-    job.start();
+    // const job = nodeCron.schedule("30 20 * * * *", async () => {
+    //   await crawlEvents(scheduleTime);
+    // });
+    // job.start();
   });
 }
 
-export default app;
+export default server;
